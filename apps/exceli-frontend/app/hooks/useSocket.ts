c562ab408ba98c1a1e0a5228ba9678e10dceb472
cmmohitchau <cmmohitchau@gmail.com>
2025-07-08 21:44:20 +0530

    deleted unnecessary line

--- apps/exceli-frontend/app/hooks/useSocket.ts
+++ apps/exceli-frontend/app/hooks/useSocket.ts
@@ -1,21 +1,35 @@
 import { WS_URL } from "@repo/common/urls";
-import axios from "axios";
 import { useEffect, useState } from "react";
 
-export function useSocket() {
+export function useSocket(id : number) {
 
     const [loading , setLoading] = useState(true);
     const [socket , setSocket] = useState<WebSocket | null>(null);
 
 
+
     useEffect( () => {
         const token = localStorage.getItem("token");
+        console.log("roomId in use socket : " , id);
+        
+        
+        
         const ws = new WebSocket(`${WS_URL}?token=${token}`);
 
+        
         ws.onopen = () => {
             setSocket(ws);
-            setLoading(false);
-        }
+            
+            const data = JSON.stringify({
+                type : "join_room",
+                RoomId :id
+            });
+            ws.send(data);
+            setLoading(false);            
+        }        
+        return () => {
+        ws.close();
+    };
     } , []);
 
     return {loading , socket};
