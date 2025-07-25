0a7811fde5e458cc30c7d0e53d18986d9354e719
cmmohitchau <cmmohitchau@gmail.com>
2025-07-25 11:28:00 +0530

    added button ui component

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,18 +1,18 @@
 "use client";
 
-import { ReactNode } from "react";
+import { MouseEventHandler, ReactNode } from "react";
 
-interface ButtonProps {
+interface ButtonProps   {
   children: ReactNode;
   className?: string;
-  appName: string;
+  onclick : MouseEventHandler<HTMLButtonElement>
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({ children, className , onclick  }: ButtonProps) => {
   return (
     <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      onClick={onclick}
     >
       {children}
     </button>
